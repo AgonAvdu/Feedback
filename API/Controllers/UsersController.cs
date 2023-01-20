3c1581fd3f50d7fbddd54bd54c223c369550b37b
Sadete Dragaj <infosys.dragaj@extaccount.com>
2023-01-20 10:07:32 +0100

    Create databse

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,14 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
 
-    public class UsersController 
+    public class UsersController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -16,9 +17,18 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUsers>> GetUsers() 
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users = _
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+         public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+
         }
     }
 }
